8d5df05d7dec3be057ef1e19a174e9a1d369a4ca
Jeremy Soller <jeremy@system76.com>
2019-06-13 14:29:53 -0600

    Add code to attempt to enable GPU, when configured

--- src/mainboard/system76/cfl-h/ramstage.c
+++ src/mainboard/system76/cfl-h/ramstage.c
@@ -14,10 +14,43 @@
  */
 
 #include <console/console.h>
+#include <delay.h>
 #include <device/device.h>
+#include <gpio.h>
 #include <pc80/keyboard.h>
 #include <soc/ramstage.h>
 #include <variant/gpio.h>
+
+#ifdef GPU_RESET
+
+#define DGPU_RSTN GPP_F22
+#define DGPU_PWR_EN GPP_F23
+#define DGPU_PWRGD GPP_K22
+
+static void gpu_reset(void) {
+	// Set DGPU_PWR_EN and Wait for DGPU_PWRGD
+	printk(BIOS_INFO, "system76: DGPU reset start\n");
+	gpio_set(DGPU_RSTN, 0);
+
+	printk(BIOS_INFO, "system76: DGPU disable power\n");
+	gpio_set(DGPU_PWR_EN, 0);
+	while (gpio_get(DGPU_PWRGD)) {
+		printk(BIOS_INFO, "system76: DGPU wait for disabled power\n");
+		mdelay(4);
+	}
+
+	printk(BIOS_INFO, "system76: DGPU enable power\n");
+	gpio_set(DGPU_PWR_EN, 1);
+	while (! gpio_get(DGPU_PWRGD)) {
+		printk(BIOS_INFO, "system76: DGPU wait for enabled power\n");
+		mdelay(4);
+	}
+
+	printk(BIOS_INFO, "system76: DGPU reset finished\n");
+	gpio_set(DGPU_RSTN, 1);
+}
+
+#endif
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params) {
 	/* Configure pads prior to SiliconInit() in case there's any
@@ -31,6 +64,10 @@ static void mainboard_init(struct device *dev) {
 }
 
 static void mainboard_enable(struct device *dev) {
+#ifdef GPU_RESET
+	gpu_reset();
+#endif
+
 	dev->ops->init = mainboard_init;
 }
 
