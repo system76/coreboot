547ba5e927f54d6f190812a2aeeca46e9134af49
Jeremy Soller <jeremy@system76.com>
2020-08-03 12:37:00 -0600

    Data fabric init
    Change-Id: I861f1d494340cb1b1f2af8f0fdb7001421b1173a

--- src/soc/amd/matisse/data_fabric.c
+++ src/soc/amd/matisse/data_fabric.c
@@ -37,6 +37,130 @@ static int find_unused_mmio_reg(void)
 	return -1;
 }
 
+struct data_fabric_range {
+	uint8_t pci_base;
+	uint8_t pci_limit;
+	bool pci_read;
+	bool pci_write;
+	uint16_t pio_base;
+	uint16_t pio_limit;
+	bool pio_read;
+	bool pio_write;
+	uint64_t mmio_base;
+	uint64_t mmio_limit;
+	bool mmio_read;
+	bool mmio_write;
+};
+
+// Basically do what AGESA should be doing. I/O memory is all of this space:
+// PIO: 0x0000 - 0xFFFF
+// MMIO: 0xC0000000 - 0xFFFFFFFF
+const struct data_fabric_range RANGES[NUM_NB_MMIO_REGS] = {
+	// DF0, Standard IO space, all read/write
+	{
+		.pci_base = 0x00,
+		.pci_limit = 0xFF,
+		.pci_read = true,
+		.pci_write = true,
+		.pio_base = 0x0000,
+		.pio_limit = 0xFFFF,
+		.pio_read = true,
+		.pio_write = true,
+		.mmio_base = 0xC0000000,
+		.mmio_limit = 0xFFFFFFFF,
+		.mmio_read = true,
+		.mmio_write = true,
+	},
+	// DF1, Unset
+	{0},
+	// DF2, Unset
+	{0},
+	// DF3, Unset
+	{0},
+	// DF4, Unset
+	{0},
+	// DF5, Unset
+	{0},
+	// DF6, Unset
+	{0},
+	// DF7, Unset
+	{0},
+};
+
+static void data_fabric_range_set(unsigned int reg, const struct data_fabric_range * range) {
+	printk(BIOS_DEBUG, "data_fabric_range_set %d\n", reg);
+
+	// Do not print while a range is disabled, you may lose access
+
+	// PCI range
+	{
+		uint32_t map =
+			(((uint32_t)range->pci_limit) << 24) |
+			(((uint32_t)range->pci_base) << 16) |
+			(IOMS0_FABRIC_ID << 4) |
+			(((uint32_t)range->pci_write) << 1) |
+			(((uint32_t)range->pci_read) << 0);
+		printk(BIOS_DEBUG, "  pci map 0x%X\n", map);
+
+		//TODO: Does it need to be disabled first?
+		pci_write_config32(SOC_DF_F0_DEV, NB_PCI_MAP(reg), map);
+	}
+
+	// PIO range
+	{
+		uint32_t base =
+			(((uint32_t)range->pio_base) << 12) |
+			(((uint32_t)range->pio_write) << 1) |
+			(((uint32_t)range->pio_read) << 0);
+		uint32_t limit =
+			(((uint32_t)range->pio_limit) << 12) |
+			IOMS0_FABRIC_ID;
+		printk(BIOS_DEBUG, "  pio base 0x%X\n", base);
+		printk(BIOS_DEBUG, "  pio limit 0x%X\n", limit);
+
+		//TODO: Does it need to be disabled first?
+		pci_write_config32(SOC_DF_F0_DEV, NB_PIO_BASE(reg), base);
+		pci_write_config32(SOC_DF_F0_DEV, NB_PIO_LIMIT(reg), limit);
+	}
+
+	// MMIO range
+	{
+		//TODO: make sure mmio_base and mmio_limit have no invalid bits
+		uint32_t base =
+			(uint32_t)(range->mmio_base >> D18F0_MMIO_SHIFT);
+		uint32_t limit =
+			(uint32_t)(range->mmio_limit >> D18F0_MMIO_SHIFT);
+		uint32_t ctrl =
+			(((uint32_t)range->mmio_write) << 1) |
+			(((uint32_t)range->mmio_read) << 0) |
+			(IOMS0_FABRIC_ID << MMIO_DST_FABRIC_ID_SHIFT);
+
+		printk(BIOS_DEBUG, "  mmio base 0x%X\n", base);
+		printk(BIOS_DEBUG, "  mmio limit 0x%X\n", limit);
+		printk(BIOS_DEBUG, "  mmio ctrl 0x%X\n", ctrl);
+
+		// Disable the mmio range
+		pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_CONTROL(reg), IOMS0_FABRIC_ID << MMIO_DST_FABRIC_ID_SHIFT);
+
+		// Set base and limit
+		pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_BASE(reg), base);
+		pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_LIMIT(reg), limit);
+
+		// Enable the range
+		pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_CONTROL(reg), ctrl);
+	}
+}
+
+static void data_fabric_init(void) {
+	printk(BIOS_DEBUG, "data_fabric_init start\n");
+
+	for (unsigned int reg = 0; reg < NUM_NB_MMIO_REGS; reg++) {
+		data_fabric_range_set(reg, &RANGES[reg]);
+	}
+
+	printk(BIOS_DEBUG, "data_fabric_init done\n");
+}
+
 void data_fabric_set_mmio_np(void)
 {
 	/*
@@ -58,31 +182,45 @@ void data_fabric_set_mmio_np(void)
 	 *     LAPIC addresses being sufficiently large that the shifted limits
 	 *     +/-1 are always equivalent to the non-shifted values +/-1.
 	 */
+	printk(BIOS_DEBUG, "data_fabric_set_mmio_np start\n");
+
+	data_fabric_init();
 
 	unsigned int i;
 	int reg;
 	uint32_t base, limit, ctrl;
 	const uint32_t np_bot = HPET_BASE_ADDRESS >> D18F0_MMIO_SHIFT;
 	const uint32_t np_top = (LOCAL_APIC_ADDR - 1) >> D18F0_MMIO_SHIFT;
+	printk(BIOS_DEBUG, " np_bot: 0x%X, np_top: 0x%X\n", np_bot, np_top);
 
 	for (i = 0; i < NUM_NB_MMIO_REGS; i++) {
+		printk(BIOS_DEBUG, " df %d\n", i);
+
 		/* Adjust all registers that overlap */
 		ctrl = pci_read_config32(SOC_DF_F0_DEV, NB_MMIO_CONTROL(i));
+		printk(BIOS_DEBUG, "  ctrl 0x%X\n", ctrl);
 		if (!(ctrl & (MMIO_WE | MMIO_RE)))
 			continue; /* not enabled */
 
 		base = pci_read_config32(SOC_DF_F0_DEV, NB_MMIO_BASE(i));
+		printk(BIOS_DEBUG, "  base 0x%X\n", base);
 		limit = pci_read_config32(SOC_DF_F0_DEV, NB_MMIO_LIMIT(i));
+		printk(BIOS_DEBUG, "  limit 0x%X\n", limit);
 
-		if (base > np_top || limit < np_bot)
+		if (base > np_top || limit < np_bot) {
+			printk(BIOS_DEBUG, "  no overlap at all\n");
 			continue; /* no overlap at all */
+		}
 
 		if (base >= np_bot && limit <= np_top) {
+			printk(BIOS_DEBUG, "  completely within, so remove\n");
 			disable_mmio_reg(i); /* 100% within, so remove */
 			continue;
 		}
 
 		if (base < np_bot && limit > np_top) {
+			printk(BIOS_DEBUG, "  split the region\n");
+
 			/* Split the configured region */
 			pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_LIMIT(i), np_bot - 1);
 			reg = find_unused_mmio_reg();
@@ -101,13 +239,17 @@ void data_fabric_set_mmio_np(void)
 		}
 
 		/* If still here, adjust only the base or limit */
-		if (base <= np_bot)
+		if (base <= np_bot) {
+			printk(BIOS_DEBUG, "  adjust limit\n");
 			pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_LIMIT(i), np_bot - 1);
-		else
+		} else {
+			printk(BIOS_DEBUG, "  adjust base\n");
 			pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_BASE(i), np_top + 1);
+		}
 	}
 
 	reg = find_unused_mmio_reg();
+	printk(BIOS_DEBUG, " find_unused_mmio_reg: 0x%X\n", reg);
 	if (reg < 0) {
 		printk(BIOS_ERR, "Error: cannot configure region as NP\n");
 		return;
@@ -118,6 +260,8 @@ void data_fabric_set_mmio_np(void)
 	pci_write_config32(SOC_DF_F0_DEV, NB_MMIO_CONTROL(reg),
 			   (IOMS0_FABRIC_ID << MMIO_DST_FABRIC_ID_SHIFT) | MMIO_NP | MMIO_WE
 				   | MMIO_RE);
+
+	printk(BIOS_DEBUG, "data_fabric_set_mmio_np finish\n");
 }
 
 static const char *data_fabric_acpi_name(const struct device *dev)
