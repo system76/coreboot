44f336501bc609e1ca46f0459ab59efb088ed540
Jeremy Soller <jeremy@system76.com>
2020-08-03 12:22:39 -0600

    Fix data fabric defines
    Change-Id: I1dd9c37b67a5249fdac422416d13ae1ff3c72680

--- src/soc/amd/matisse/include/soc/data_fabric.h
+++ src/soc/amd/matisse/include/soc/data_fabric.h
@@ -6,16 +6,24 @@
 #include <types.h>
 
 /* D18F0 - Fabric Configuration registers */
-#define IOMS0_FABRIC_ID			9
+#define IOMS0_FABRIC_ID			4
 
 #define D18F0_VGAEN			0x80
 #define   VGA_ADDR_ENABLE		BIT(0)
+
+#define D18F0_PCI_MAP0	    0xA0
+#define NB_PCI_MAP(reg)		((reg) * sizeof(uint32_t) + D18F0_PCI_MAP0)
+
+#define D18F0_PIO_BASE0		0xC0
+#define D18F0_PIO_LIMIT0	0xC4
+#define NB_PIO_BASE(reg)		((reg) * 2 * sizeof(uint32_t) + D18F0_PIO_BASE0)
+#define NB_PIO_LIMIT(reg)		((reg) * 2 * sizeof(uint32_t) + D18F0_PIO_LIMIT0)
 
 #define D18F0_MMIO_BASE0		0x200
 #define D18F0_MMIO_LIMIT0		0x204
 #define   D18F0_MMIO_SHIFT		16
 #define D18F0_MMIO_CTRL0		0x208
-#define   MMIO_NP			BIT(12)
+#define   MMIO_NP			BIT(16)
 #define   MMIO_DST_FABRIC_ID_SHIFT	4
 #define   MMIO_WE			BIT(1)
 #define   MMIO_RE			BIT(0)
