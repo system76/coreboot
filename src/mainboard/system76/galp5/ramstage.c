3c75673da282c89541d5836f1f263a98d4ef08c1
Jeremy Soller <jeremy@system76.com>
2020-10-14 20:14:03 -0600

    Adjust GPIO init to look like prior boards
    Change-Id: I36ff193a1d540f1723f45ebd7326a02b24c090d7

---
+++ src/mainboard/system76/galp5/ramstage.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <soc/ramstage.h>
+#include "gpio.h"
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params) {
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+}
