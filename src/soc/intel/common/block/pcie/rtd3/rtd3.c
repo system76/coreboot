9747417290487799073b36a8a6acf1bb0f9f9ef6
Jeremy Soller <jeremy@system76.com>
2020-12-29 11:21:39 -0700

    intel/block/pcie/rtd3: ACPI debug messages
    Change-Id: Icc4a882ff73f62a134b92f1afb0dc298ea809189

--- src/soc/intel/common/block/pcie/rtd3/rtd3.c
+++ src/soc/intel/common/block/pcie/rtd3/rtd3.c
@@ -80,6 +80,8 @@ pcie_rtd3_acpi_method_on(unsigned int pcie_rp,
 {
 	acpigen_write_method_serialized("_ON", 0);
 
+	acpigen_write_debug_string("PCIe RTD3 _ON");
+
 	/* Assert enable GPIO to turn on device power. */
 	if (config->enable_gpio.pin_count) {
 		acpigen_enable_tx_gpio(&config->enable_gpio);
@@ -110,6 +112,8 @@ pcie_rtd3_acpi_method_off(int pcie_rp,
 			  const struct soc_intel_common_block_pcie_rtd3_config *config)
 {
 	acpigen_write_method_serialized("_OFF", 0);
+
+	acpigen_write_debug_string("PCIe RTD3 _OFF");
 
 	/* Trigger L23 ready entry flow unless disabled by config. */
 	if (!config->disable_l23)
@@ -215,8 +219,8 @@ static void pcie_rtd3_acpi_fill_ssdt(const struct device *dev)
 	/* Port number is 1-based, PMC IPC method expects 0-based. */
 	pcie_rp--;
 
-	printk(BIOS_INFO, "%s: Enable RTD3 for %s (%s)\n", scope, dev_path(parent),
-	       config->desc ?: dev->chip_ops->name);
+	printk(BIOS_INFO, "%s: Enable RTD3 for %s (%s) on RP #%d\n", scope, dev_path(parent),
+	       config->desc ?: dev->chip_ops->name, pcie_rp + 1);
 
 	/* The RTD3 power resource is added to the root port, not the device. */
 	acpigen_write_scope(scope);
