32f3311f3e4b891fea3aad5dac8cff53c2a26db7
Jeremy Soller <jeremy@system76.com>
2021-09-14 10:36:50 -0600

    Add oryp8
    Change-Id: I28611cf59c9509726af250411a867a1898ec7ace

---
+++ src/mainboard/system76/oryp8/ramstage.c
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <soc/ramstage.h>
+#include "gpio.h"
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	// PEG0 Config
+	params->CpuPcieRpAdvancedErrorReporting[0] = 0;
+	params->CpuPcieRpLtrEnable[0] = 1;
+	params->CpuPcieRpPtmEnabled[0] = 0;
+
+	// PEG1 Config
+	params->CpuPcieRpAdvancedErrorReporting[1] = 0;
+	params->CpuPcieRpLtrEnable[1] = 1;
+	params->CpuPcieRpPtmEnabled[1] = 0;
+
+	// IOM config
+	params->PchUsbOverCurrentEnable = 0;
+	params->PortResetMessageEnable[8] = 1; // TYPEC1
+	params->UsbTcPortEn = 1;
+
+	// Low latency legacy I/O
+	params->PchLegacyIoLowLatency = 1;
+
+	gpio_configure_pads(gpio_table, ARRAY_SIZE(gpio_table));
+}
