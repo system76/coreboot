e88d4b43015269942c7f7c0fae6b129a41bcf982
Jeremy Soller <jeremy@system76.com>
2021-11-09 15:23:50 -0700

    Remove ESPI config and enable LPC decodes for EC command and debug
    Change-Id: I301829fc67fc1b255cf1a6f468a5433b64e03828

--- src/mainboard/system76/kudu6/bootblock.c
+++ src/mainboard/system76/kudu6/bootblock.c
@@ -1,9 +1,32 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <bootblock_common.h>
+#include <amdblocks/lpc.h>
+#include <console/console.h>
 #include "gpio.h"
 
 void bootblock_mainboard_early_init(void)
 {
+	post_code(0x76);
+	post_code(0x01);
+
 	mainboard_program_early_gpios();
+
+	post_code(0x76);
+	post_code(0x02);
+
+	// AP/EC command
+	lpc_set_wideio_range(0xE00, 256);
+
+	// AP/EC debug
+	lpc_set_wideio_range(0xF00, 256);
+
+	post_code(0x76);
+	post_code(0x03);
+}
+
+void bootblock_mainboard_init(void)
+{
+	post_code(0x76);
+	post_code(0x10);
 }
