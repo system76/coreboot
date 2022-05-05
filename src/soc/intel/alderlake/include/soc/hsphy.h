392021e8619d8ca476e94a78d4f35e804a16f50e
Michał Żygowski <michal.zygowski@3mdeb.com>
2022-05-05 13:21:01 +0200

    soc/intel/alderlake/hsphy: Add support for HSPHY firmware loading
    BIOS must send the IP_LOAD HECI command to fetch the firmware for CPU
    PCIe Gen5 and upload it via CPU REG BAR prior FSP Silicon Init.
    Implementation based on Slimbootloader's
    "Silicon/AlderlakePkg/Library/CpuPcieHsPhyInitLib" and FSP source.
    
    TEST=Boot MSI PRO Z690-A and see the HSPHY FW is loaded and its recipe
    and version is printed.
    
    Signed-off-by: Michał Żygowski <michal.zygowski@3mdeb.com>
    Change-Id: I6c6c11581e3d3d9bab0131fae6ef487cafe98080

---
+++ src/soc/intel/alderlake/include/soc/hsphy.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_ALDERLAKE_HSPHY_H_
+#define _SOC_ALDERLAKE_HSPHY_H_
+
+void load_and_init_hsphy(void);
+void print_hsphy_version(uint32_t cpu_pcie_dev);
+
+#endif /* _SOC_ALDERLAKE_HSPHY_H_ */
