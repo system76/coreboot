d063f80fe887312951f1dca8574202e05802f3dd
Jeremy Soller <jackpot51@gmail.com>
2022-05-25 12:22:16 -0600

    mb/system76/gaze17: Add pin muxing
    Change-Id: I82b94c0e30471af65b0a6e58001c18edfe88e923

--- src/mainboard/system76/gaze17/ramstage.c
+++ src/mainboard/system76/gaze17/ramstage.c
@@ -5,5 +5,11 @@
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
+	params->CnviRfResetPinMux = 0x194CE404; // GPP_F4
+	params->CnviClkreqPinMux = 0x394CE605; // GPP_F5
+
+	params->PchSerialIoI2cSclPinMux[0] = 0x1947a405; // GPP_H5
+	params->PchSerialIoI2cSdaPinMux[0] = 0x1947c404; // GPP_H4
+
 	variant_configure_gpios();
 }
