ff79195b49c36730cb57f852214cfacfcf0f3f3a
Tim Crawford <tcrawford@system76.com>
2022-06-30 10:20:33 -0600

    soc/intel/alderlake: Use channel 0 for memory-down
    Fixes detection of the on-board RAM (Samsung K4AAG165WA-BCWE) on the
    System76 Lemur Pro 11 (lemp11).
    
    Change-Id: I216b7b67c2674e0a14fdbbf96776759e55d0ec09
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/soc/intel/alderlake/meminit.c
+++ src/soc/intel/alderlake/meminit.c
@@ -54,8 +54,8 @@ static const struct soc_mem_cfg soc_mem_cfg[] = {
 			 * configuration.
 			 */
 			.half_channel = BIT(0),
-			/* In mixed topologies, channel 1 is always memory-down. */
-			.mixed_topo = BIT(1),
+			/* In mixed topologies, channel 0 is always memory-down. */
+			.mixed_topo = BIT(0),
 		},
 	},
 	[MEM_TYPE_DDR5] = {
