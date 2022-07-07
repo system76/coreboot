682621fa1f609d99e8596de7cb120f5a139b497a
Tim Crawford <tcrawford@system76.com>
2022-07-07 14:35:15 -0600

    mb/system76/gaze17: Update 3050 variant
    Change-Id: I8d8bb8345816a039ed5bbe7ca74a122cd6005960
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/gaze17/variants/3050/gpio_early.c
+++ src/mainboard/system76/gaze17/variants/3050/gpio_early.c
@@ -4,8 +4,10 @@
 #include <soc/gpio.h>
 
 static const struct pad_config early_gpio_table[] = {
-	PAD_CFG_GPO(GPP_E4, 0, DEEP), // DGPU_PWR_EN
+	PAD_CFG_GPO(GPP_A14, 0, DEEP), // DGPU_PWR_EN
 	PAD_CFG_GPO(GPP_B2, 0, DEEP), // DGPU_RST#_PCH
+	PAD_CFG_NF(GPP_H10, NONE, DEEP, NF1), // UART0_RX
+	PAD_CFG_NF(GPP_H11, NONE, DEEP, NF1), // UART0_TX
 };
 
 void variant_configure_early_gpios(void)
