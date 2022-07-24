b17a77111a315142d94ca1fd0da3e72a23d72b4c
Jeremy Soller <jeremy@system76.com>
2022-07-24 15:16:59 -0600

    mb/system76/gaze17: Add Gazelle 17
    The gaze17 comes in 2 variants due to differences in the discrete GPU
    and network controller used.
    
    - NVIDIA RTX 3050, using Realtek Ethernet Controller
    - NVIDIA RTX 3060, using onboard I219-V Ethernet Controller
    
    Tested with a custom TianoCore UefiPayloadPkg payload.
    
    Working:
    
    - PS/2 keyboard, touchpad
    - Both DIMM slots
    - M.2 NVMe SSD
    - M.2 SATA SSD
    - MicroSD card reader
    - All USB ports
    - Webcam
    - Ethernet
    - WiFi/Bluetooth
    - Integrated graphics using Intel GOP driver
    - Internal microphone
    - Internal speakers
    - Combined headphone + mic 3.5mm audio
    - 3.5mm microphone input
    - S0ix suspend/resume
    - Booting to Pop!_OS Linux 22.04 with kernel 5.18.10
    - Internal flashing with flashrom v1.2-703-g76118a7c10ed
    
    Not working:
    
    - Discrete/Hybrid graphics: Requires NVIDIA driver
    - mDP/HDMI displays on 3060 variant: Requires NVIDIA driver
    - Detection of devices in TBT slot on boot
    - S3 suspend: MP init eventually fails
    
    Not tested:
    
    - Thunderbolt devices
    
    Change-Id: Ib12ac47e8f34004f72e6234039823530511baea7
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/gaze17/ramstage.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <smbios.h>
+#include <soc/ramstage.h>
+#include <variant/gpio.h>
+
+smbios_wakeup_type smbios_system_wakeup_type(void)
+{
+	return SMBIOS_WAKEUP_TYPE_POWER_SWITCH;
+}
+
+void mainboard_silicon_init_params(FSP_S_CONFIG *params)
+{
+	params->CnviRfResetPinMux = 0x194CE404; // GPP_F4
+	params->CnviClkreqPinMux = 0x394CE605; // GPP_F5
+
+	params->PchSerialIoI2cSdaPinMux[0] = 0x1947c404; // GPP_H4
+	params->PchSerialIoI2cSclPinMux[0] = 0x1947a405; // GPP_H5
+	params->PchSerialIoI2cSdaPinMux[1] = 0x1947c606; // GPP_H6
+	params->PchSerialIoI2cSclPinMux[1] = 0x1947a607; // GPP_H7
+
+	params->SataPortDevSlpPinMux[0] = 0x59673e0c; // GPP_H12
+	params->SataPortDevSlpPinMux[1] = 0x5967400d; // GPP_H13
+
+	variant_configure_gpios();
+}
