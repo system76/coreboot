20db8fe3c3d5997ccd242037a8296d08cb56f74b
Tim Crawford <tcrawford@system76.com>
2022-08-17 10:16:53 -0600

    mb/system76: Set SMBIOS wakeup type to power switch
    Change-Id: I87c4f1d17fcf43563a6f450799d917786efa1f2f
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/gaze16/ramstage.c
+++ src/mainboard/system76/gaze16/ramstage.c
@@ -1,8 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <smbios.h>
 #include <soc/ramstage.h>
 #include <variant/gpio.h>
 #include "variant.h"
+
+smbios_wakeup_type smbios_system_wakeup_type(void)
+{
+	return SMBIOS_WAKEUP_TYPE_POWER_SWITCH;
+}
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
