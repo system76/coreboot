56d9711a081459a7b68bf4c725b16abab9e51f10
Tim Crawford <tcrawford@system76.com>
2022-08-29 12:31:05 -0600

    mb/system76/adl-p: oryp10: Configure meminit
    - Enable early command training to fix FSP-M init of 8 GB DIMMs
    - Preserve FSP-M default of 1 for LpDdrDqDqsReTraining
    
    Change-Id: Iee6eccc6545f2920514018eff163e690f5ab6c01
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/adl-p/variants/oryp10/romstage.c
+++ src/mainboard/system76/adl-p/variants/oryp10/romstage.c
@@ -10,6 +10,8 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 	const struct mb_cfg board_cfg = {
 		.type = MEM_TYPE_DDR5,
 		.rcomp = { .resistor = 100, },
+		.ect = true,
+		.LpDdrDqDqsReTraining = 1,
 	};
 	const struct mem_spd spd_info = {
 		.topo = MEM_TOPO_DIMM_MODULE,
