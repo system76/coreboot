f385486bebf5d18421063d1ccf0f566cd1fe1e77
Tim Crawford <tcrawford@system76.com>
2023-02-06 15:28:20 -0700

    mb/system76/adl: Add Oryx Pro 11 as a variant
    Change-Id: Ib9cf93dc88cd51d429222540ce0721c6e5f4013a
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/adl/variants/oryp11/include/variant/gpio.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <soc/gpio.h>
+
+#define DGPU_RST_N	GPP_R16
+#define DGPU_PWR_EN	GPP_F9
+#define DGPU_GC6	GPP_A11
+#define DGPU_SSID	0xd5021558
+
+#endif
