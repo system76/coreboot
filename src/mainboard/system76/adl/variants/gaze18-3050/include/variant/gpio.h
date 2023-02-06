d2fc079b7181a8e4647eab43a67718cbb63ff8c8
Tim Crawford <tcrawford@system76.com>
2023-02-06 10:42:28 -0700

    mb/system76/adl: Add Gazelle 18 (3050) as a variant
    There will be two versions of firmware for the Gazelle 18:
    
    - One for units using NVIDIA RTX 3050
    - One for units using NVIDIA RTX 40x0
    
    Change-Id: I7c1a51fda012fe4e7a43bbe5ef98a3aec38373a0
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/adl/variants/gaze18-3050/include/variant/gpio.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <soc/gpio.h>
+
+#define DGPU_RST_N
+#define DGPU_PWR_EN
+#define DGPU_GC6
+#define DGPU_SSID
+
+#endif
