6e5fbf566f0f06896b1414d3898730789fd6bebd
Jeremy Soller <jackpot51@gmail.com>
2023-02-10 12:43:57 -0700

    gaze18-40x0: enable NVIDIA GPU
    Change-Id: Iefb6de5bdc481934d7c661516c05ee945e8717db

--- src/mainboard/system76/rpl/variants/gaze18-40x0/gpio_early.c
+++ src/mainboard/system76/rpl/variants/gaze18-40x0/gpio_early.c
@@ -6,6 +6,8 @@
 static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_NF(GPP_C20, NONE, DEEP, NF1), // UART2_RXD
 	PAD_CFG_NF(GPP_C21, NONE, DEEP, NF1), // UART2_TXD
+	PAD_CFG_GPO(GPP_F9, 0, DEEP), // DGPU_PWR_EN
+	PAD_CFG_GPO(GPP_R16, 0, DEEP), // DGPU_RST#_PCH
 };
 
 void mainboard_configure_early_gpios(void)
