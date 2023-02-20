6846f84bc5f56c78635ff5e2bf0c20f3e0d4006c
Tim Crawford <tcrawford@system76.com>
2023-02-20 11:45:29 -0700

    mb/system76/rpl: gaze18-3050: Document GPIOs
    Change-Id: I5a74d0d8bf752d081975cd26ddf9d5b749c0ae9b
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/rpl/variants/gaze18-3050/gpio_early.c
+++ src/mainboard/system76/rpl/variants/gaze18-3050/gpio_early.c
@@ -4,6 +4,8 @@
 #include <soc/gpio.h>
 
 static const struct pad_config early_gpio_table[] = {
+	PAD_CFG_GPO(GPP_A14, 0, DEEP), // DGPU_PWR_EN
+	PAD_CFG_GPO(GPP_B2, 1, DEEP), // DGPU_RST#_PCH
 	PAD_CFG_NF(GPP_H10, NONE, DEEP, NF1), // UART0_RX
 	PAD_CFG_NF(GPP_H11, NONE, DEEP, NF1), // UART0_TX
 };
