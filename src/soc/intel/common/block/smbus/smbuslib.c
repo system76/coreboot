bf275b38606c94a107f3a34f1defbb95c771e030
Jeremy Soller <jackpot51@gmail.com>
2023-05-16 15:27:30 -0600

    Fix DDR5 SPD length
    Change-Id: Id587c63a4abafc3f47176471447f79c0f57729f2

--- src/soc/intel/common/block/smbus/smbuslib.c
+++ src/soc/intel/common/block/smbus/smbuslib.c
@@ -25,7 +25,7 @@ static void update_spd_len(struct spd_block *blk)
 			j |= blk->spd_array[i][SPD_DRAM_TYPE];
 
 	/* If spd used is DDR4, then its length is 512 byte. */
-	if (j == SPD_DRAM_DDR4)
+	if (j == SPD_DRAM_DDR4 || j == SPD_DRAM_DDR5)
 		blk->len = SPD_PAGE_LEN_DDR4;
 	else
 		blk->len = SPD_PAGE_LEN;
