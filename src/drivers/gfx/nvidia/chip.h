53968e2e88f79ce88f77024b3a6eca9cd3909995
Jeremy Soller <jeremy@system76.com>
2023-06-27 09:52:22 -0600

    drivers/gfx/nvidia: Add driver for NVIDIA GPU
    Add a driver for laptops with NVIDIA Optimus (hybrid) graphics. The
    driver provides ACPI support for dynamically powering on and off the
    GPU, NVIDIA Dynamic Boost support, and a function for enabling the GPU
    power in romstage.
    
    References:
    - DG-09845-001: NVIDIA GN20/QN20 Hardware Design Guide
    - DG-09954-001: NVIDIA GN20/QN20 Software Design Guide
    
    Change-Id: I2dec7aa2c8db7994f78a7cc1220502676e248465
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/drivers/gfx/nvidia/chip.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _DRIVERS_GFX_NVIDIA_CHIP_H_
+#define _DRIVERS_GFX_NVIDIA_CHIP_H_
+
+struct drivers_gfx_nvidia_config {
+	/* TODO: Set GPIOs in devicetree? */
+};
+
+#endif /* _DRIVERS_GFX_NVIDIA_CHIP_H_ */
