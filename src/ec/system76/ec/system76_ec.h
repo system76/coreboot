206bb54c13c3d0430fb37fe73ff595942e051dcc
Tim Crawford <tcrawford@system76.com>
2023-11-20 07:24:17 -0700

    ec/system76: Support lockdown based on EC security state
    Change-Id: I202c0607c2cdac1df59f42fb41735704dd5bd95c
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/ec/system76/ec/system76_ec.h
+++ src/ec/system76/ec/system76_ec.h
@@ -6,6 +6,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+// Default value, flashing is prevented, cannot be set with CMD_SECURITY_SET
+#define SYSTEM76_EC_SECURITY_STATE_LOCK 0
+// Flashing is allowed, cannot be set with CMD_SECURITY_SET
+#define SYSTEM76_EC_SECURITY_STATE_UNLOCK 1
+// Flashing will be prevented on the next reboot
+#define SYSTEM76_EC_SECURITY_STATE_PREPARE_LOCK 2
+// Flashing will be allowed on the next reboot
+#define SYSTEM76_EC_SECURITY_STATE_PREPARE_UNLOCK 3
+
 /*
  * Send a command to the EC.  request_data/request_size are the request payload,
  * request_data can be NULL if request_size is 0.  reply_data/reply_size are
@@ -13,5 +22,7 @@
  */
 bool system76_ec_cmd(uint8_t cmd, const uint8_t *request_data,
 	uint8_t request_size, uint8_t *reply_data, uint8_t reply_size);
+
+bool system76_ec_security_get(uint8_t *state);
 
 #endif
