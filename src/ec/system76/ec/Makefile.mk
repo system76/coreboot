bb54e49a54618bf2f4f48114c0a4c4fce607b736
Tim Crawford <tcrawford@system76.com>
2023-11-20 07:24:17 -0700

    ec/system76: Support lockdown based on EC security state
    Change-Id: I202c0607c2cdac1df59f42fb41735704dd5bd95c
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/ec/system76/ec/Makefile.mk
+++ src/ec/system76/ec/Makefile.mk
@@ -4,6 +4,7 @@ ifeq ($(CONFIG_EC_SYSTEM76_EC),y)
 all-y += system76_ec.c
 
 ramstage-y += smbios.c
+ramstage-$(CONFIG_EC_SYSTEM76_EC_LOCKDOWN) += lockdown.c
 
 smm-$(CONFIG_DEBUG_SMI) += system76_ec.c
 
