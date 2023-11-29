b0e394466fab4093e64203d65caece6d7b0470b6
Tim Crawford <tcrawford@system76.com>
2023-11-29 12:06:59 -0700

    [WIP] ec/system76/ec: Implement usbc_get_ops
    Change-Id: Icee0661ec0d0c949b73503e639dd8a461c05235a
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/ec/system76/ec/usbc_mux.c
@@ -0,0 +1,59 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/usbc_mux.h>
+
+static int system76_ec_get_mux_info(int port, struct usbc_mux_info *info)
+{
+	if (!info)
+		return -1;
+
+	// TODO: Get actual TBT port info
+	info->dp = false;
+	info->usb = true;
+	info->cable = false;
+	info->polarity = false;
+	info->hpd_lvl = false;
+	info->hpd_irq = false;
+	info->ufp = false;
+	info->dbg_acc = false;
+	info->dp_pin_mode = 0;
+
+	return 0;
+}
+
+static int system76_ec_wait_for_connection(long timeout_ms)
+{
+	return 0;
+}
+
+static int system76_ec_enter_dp_mode(int port)
+{
+	return 0;
+}
+
+static int system76_ec_wait_for_dp_mode_entry(int port, long timeout_ms)
+{
+	return 0;
+}
+
+static int system76_ec_wait_for_hpd(int port, long timeout_ms)
+{
+	return 0;
+}
+
+static const struct usbc_ops system76_ec_usbc_ops = {
+	.mux_ops = {
+		.get_mux_info = system76_ec_get_mux_info,
+	},
+	.dp_ops = {
+		.wait_for_connection = system76_ec_wait_for_connection,
+		.enter_dp_mode = system76_ec_enter_dp_mode,
+		.wait_for_dp_mode_entry = system76_ec_wait_for_dp_mode_entry,
+		.wait_for_hpd = system76_ec_wait_for_hpd,
+	},
+};
+
+const struct usbc_ops *usbc_get_ops(void)
+{
+	return &system76_ec_usbc_ops;
+}
