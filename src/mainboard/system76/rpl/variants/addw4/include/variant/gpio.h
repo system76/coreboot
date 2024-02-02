fdf2695d632938dcd18e1489fa5db331472b2daa
Tim Crawford <tcrawford@system76.com>
2024-02-02 13:20:16 -0700

    mb/system76/rpl: Add Adder WS 4 as a variant
    Change-Id: Ic9a886445d6280514d62d953765105087a6e60fb
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/rpl/variants/addw4/include/variant/gpio.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <soc/gpio.h>
+
+#define DGPU_RST_N	GPP_R16
+#define DGPU_PWR_EN	GPP_F9
+#define DGPU_SSID	0x03531558
+
+#endif
