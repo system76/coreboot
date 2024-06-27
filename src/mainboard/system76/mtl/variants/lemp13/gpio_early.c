4aaf717bd40ade79ed873c2d01447fb29df933b5
Jeremy Soller <jackpot51@gmail.com>
2024-06-27 07:25:54 -0600

    lemp13: make sure to configure GPP_E11 in romstage
    Change-Id: Iea28473c3a1dcf05e7c6701d61109b413cd04501
    Signed-off-by: Jeremy Soller <jackpot51@gmail.com>

--- src/mainboard/system76/mtl/variants/lemp13/gpio_early.c
+++ src/mainboard/system76/mtl/variants/lemp13/gpio_early.c
@@ -6,6 +6,7 @@
 static const struct pad_config early_gpio_table[] = {
 	PAD_CFG_NF(GPP_C00, NONE, DEEP, NF1), // SMB_CLK
 	PAD_CFG_NF(GPP_C01, NONE, DEEP, NF1), // SMB_DATA
+	PAD_CFG_GPI(GPP_E11, NONE, DEEP), // BOARD_ID1
 	PAD_CFG_NF(GPP_H08, NONE, DEEP, NF1), // UART0_RX
 	PAD_CFG_NF(GPP_H09, NONE, DEEP, NF1), // UART0_TX
 };
--- src/mainboard/system76/mtl/variants/lemp13/romstage.c
+++ src/mainboard/system76/mtl/variants/lemp13/romstage.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <soc/gpio.h>
 #include <soc/meminit.h>
 #include <soc/romstage.h>
 
