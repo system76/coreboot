3f38627e86554345e3ad33520a391b719c2a087b
Tim Crawford <tcrawford@system76.com>
2025-04-01 11:36:00 -0600

    mb/system76: Enable dGPUs
    Change-Id: I4ca91ff631dd4badbfba72e69651f03753323a54
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/gaze15/variants/gaze15/include/variant/gpio.h
+++ src/mainboard/system76/gaze15/variants/gaze15/include/variant/gpio.h
@@ -3,7 +3,16 @@
 #ifndef VARIANT_GPIO_H
 #define VARIANT_GPIO_H
 
+#include <soc/gpio.h>
+
+#define DGPU_RST_N	GPP_F22
+#define DGPU_PWR_EN	GPP_F23
+#define DGPU_GC6	GPP_K21
+#define DGPU_SSID	0x85201558
+
+#ifndef __ACPI__
 void variant_configure_early_gpios(void);
 void variant_configure_gpios(void);
+#endif
 
 #endif
