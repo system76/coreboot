795108b5aa5fb0e053991f1f6a78508e505810f3
Jeremy Soller <jackpot51@gmail.com>
2025-04-17 09:50:02 -0600

    mb/system76/meer9: Add Meerkat 9
    The Meerkat 9 is an Intel Meteor Lake-H based small form factor desktop
    computer based on the Asus NUC-155H R2.
    
    Change-Id: I37a0b808cf383379b8e284831644c824c0d4817e
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/meer9/ramstage.c
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/smbus_host.h>
+#include <mainboard/gpio.h>
+#include <soc/ramstage.h>
+
+static void mainboard_init(void *chip_info)
+{
+	mainboard_configure_gpios();
+
+	// The DACC feature resets CMOS and causes a system reset if the
+	// firmware does not send this message within 10 seconds of power on.
+	printk(BIOS_DEBUG, "Handling DACC\n");
+	do_smbus_write_byte(CONFIG_FIXED_SMBUS_IO_BASE, 0xBA >> 1, 0x0F, 0xAA);
+}
+
+struct chip_operations mainboard_ops = {
+	.init = mainboard_init,
+};
