system76/coreboot

commit c0ba1166e97dc29e032faf6120a18891631383a5

soc/intel/adl,mtl: Use channel 0 only for memory down in mixed topo
Change-Id: Ic30bec272e82535f6f606033c3ba512662cb2c8b
Signed-off-by: Jeremy Soller <jackpot51@gmail.com>

src/soc/intel/alderlake/meminit.c
@@ -70,8 +70,8 @@ static const struct soc_mem_cfg soc_mem_cfg[] = {
 			 * configuration.
 			 */
 			.half_channel = BIT(0),
-			/* In mixed topologies, either channel 0 or 1 can be memory-down. */
-			.mixed_topo = BIT(0) | BIT(1),
+			/* In mixed topologies, channel 0 is memory-down. */
+			.mixed_topo = BIT(0),
 		},
 	},
 	[MEM_TYPE_LP4X] = {

src/soc/intel/meteorlake/meminit.c
@@ -46,8 +46,8 @@ static const struct soc_mem_cfg soc_mem_cfg[] = {
 			 * configuration.
 			 */
 			.half_channel = BIT(0),
-			/* In mixed topologies, either channel 0 or 1 can be memory-down. */
-			.mixed_topo = BIT(0) | BIT(1),
+			/* In mixed topologies, channel 0 is memory-down. */
+			.mixed_topo = BIT(0),
 		},
 	},
 	[MEM_TYPE_LP5X] = {

commit 6982eaf6b0df86fcaf8e6f702921756c85d8ccf3

soc/intel/adl: Only use channel 0 for mem-down DDR4
Fixes: c0ba1166e97d ("soc/intel/adl,mtl: Use channel 0 only for memory down in mixed topo")
Change-Id: I56e7d51b4a06519376d8fe7a345bb434c6f1a24a
Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/soc/intel/alderlake/meminit.c
@@ -54,8 +54,8 @@ static const struct soc_mem_cfg soc_mem_cfg[] = {
 			 * configuration.
 			 */
 			.half_channel = BIT(0),
-			/* In mixed topologies, either channel 0 or 1 can be memory-down. */
-			.mixed_topo = BIT(0) | BIT(1),
+			/* In mixed topologies, channel 0 is memory-down. */
+			.mixed_topo = BIT(0),
 		},
 	},
 	[MEM_TYPE_DDR5] = {