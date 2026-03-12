2942c415db5c5934140fccd9ddfb3932bbfb172f
Jan Philipp Groß <jeangrande@mailbox.org>
2026-03-12 21:34:06 +0100

    mb/asus: Add ASUS Z87-K (Haswell)
    Based on Autoport with subsequent manual tweaking.
    The board features an unpopulated TPM header,
    so the necessary code for TPM 2.0 support was added.
    
    Working:
    - Devil's Canyon CPUs (tested with i7-4790K on MRC and NRI)
    - All four DDR3 DIMM slots (tested with 2x G.SKILL F3-1600C9-8GAR
      and 2x Kingston KHX1600C9D3 on MRC and NRI)
    - All video ports
    - All USB ports
    - Audio Jack
    - Ethernet port
    - All SATA ports
    - All PCIe slots
    - All PCI slots
    - Discrete Graphics (tested with AMD R9 Nano)
    - S3 Suspend and Resume
    
    Not (yet) tested:
    - PS/2 port
    - Power LED blinking in S3. If it doesn't work, enable blink for
      PCH GPIO24 when entering S3.
    
    Change-Id: I0846441b66e10f28cba1d0ac227dc5d757fae125
    Signed-off-by: Jan Philipp Groß <jeangrande@mailbox.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91663
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/mainboard/asus/z87-k/hda_verb.c
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+static const u32 realtek_alc887_verbs[] = {
+	AZALIA_SUBVENDOR(0, 0x10438576),
+	AZALIA_PIN_CFG(0, 0x11, 0x90460130),
+	AZALIA_PIN_CFG(0, 0x12, 0x40330000),
+	AZALIA_PIN_CFG(0, 0x14, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x15, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x16, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x17, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19040),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19050),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181304f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214020),
+	AZALIA_PIN_CFG(0, 0x1c, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1d, 0x4044c601),
+	AZALIA_PIN_CFG(0, 0x1e, 0x411111f0),
+	AZALIA_PIN_CFG(0, 0x1f, 0x411111f0),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Realtek ALC887",
+		.vendor_id    = 0x10ec0887,
+		.subsystem_id = 0x10438576,
+		.address      = 0,
+		.verbs        = realtek_alc887_verbs,
+		.verb_count   = ARRAY_SIZE(realtek_alc887_verbs),
+	},
+	{ /* terminator */ }
+};
+
+AZALIA_ARRAY_SIZES;
