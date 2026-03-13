e69bfef7c0eabda20c322eea7fadc347d0ed5076
Arthur Heymans <arthur@aheymans.xyz>
2026-03-13 13:41:54 +0100

    mb/emu/qemu-sbsa: Set io_port_mmio_base for PCI I/O port support
    The QEMU sbsa-ref machine maps PCI I/O port space at 0x7fff0000.
    Set io_port_mmio_base so that PCI I/O port accesses are correctly
    translated to MMIO on aarch64.
    
    This is needed for PCI device drivers that may use I/O port BARs,
    such as the QEMU bochs display driver which already compiles and
    links for this target via the existing DRIVERS_EMULATION_QEMU_BOCHS
    Kconfig.
    
    Change-Id: I6a06c4c3a48c5d3409009f10b089a3537ccec8a6
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91667
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- src/mainboard/emulation/qemu-sbsa/mmio.c
+++ src/mainboard/emulation/qemu-sbsa/mmio.c
@@ -3,6 +3,8 @@
 #include <console/uart.h>
 #include <mainboard/addressmap.h>
 
+uintptr_t io_port_mmio_base = SBSA_PCIE_PIO_BASE;
+
 uintptr_t uart_platform_base(unsigned int idx)
 {
 	return SBSA_UART_BASE;
