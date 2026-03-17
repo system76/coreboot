3363aa32f79ef6c47cc4269a4049f06c61cde472
Jeremy Soller <jackpot51@gmail.com>
2026-03-16 20:15:33 -0600

    mb/system76/meer9: Add Meerkat 9
    The Meerkat 9 is an Intel Meteor Lake-H based small form factor desktop
    computer based on the Asus NUC-155H R2.
    
    Change-Id: I37a0b808cf383379b8e284831644c824c0d4817e
    Signed-off-by: Jeremy Soller <jeremy@system76.com>
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/meer9/Makefile.mk
@@ -0,0 +1,13 @@
+## SPDX-License-Identifier: GPL-2.0-only
+
+CPPFLAGS_common += -I$(src)/mainboard/$(MAINBOARDDIR)/include
+
+bootblock-y += bootblock.c
+bootblock-y += variants/$(VARIANT_DIR)/gpio_early.c
+
+romstage-y += variants/$(VARIANT_DIR)/romstage.c
+
+ramstage-y += ramstage.c
+ramstage-y += variants/$(VARIANT_DIR)/hda_verb.c
+ramstage-y += variants/$(VARIANT_DIR)/gpio.c
+ramstage-y += variants/$(VARIANT_DIR)/ramstage.c
