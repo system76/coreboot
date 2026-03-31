7f3a299dc883fd1a6ce060dbd9b29f7923747a3b
Angel Pons <th3fanbus@gmail.com>
2026-03-31 18:10:45 +0200

    mb/asus/h61-series: Add Asus P8H61-M LX2
    Add support for yet another of these LGA1155 boards. Nothing too fancy,
    just another slightly different combination of the same chips.
    
    Working:
    - Booting to Arch Linux using SeaBIOS (current version)
    - Native RAM init and S3 resume
    - Realtek Ethernet NIC
    - Front and rear USB ports
    - All SATA ports
    - libgfxinit to initialise a VGA display
    - DVI-D output after booting to Linux
    - VBT (extracted from `/sys/kernel/debug/dri/0/i915_vbt`)
    - Both PCIe x1 slots
    - Internal flashing
    - Serial port to emit spam
    - PS/2 port with a keyboard
    
    Not working:
    - Automatic fan speed control (known limitation of Super I/O code)
    
    Untested:
    - PCIe x16 slot
    - EHCI debug (one of the rear USB 2.0 ports)
    - Audio
    
    Change-Id: I53c8c9376faa4cdda8e468350db5114aecdc0d7c
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92181
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ src/mainboard/asus/h61-series/variants/p8h61-m_lx2/early_init.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <bootblock_common.h>
+#include <device/pnp_ops.h>
+#include <superio/nuvoton/common/nuvoton.h>
+#include <superio/nuvoton/nct6776/nct6776.h>
+
+#define GLOBAL_DEV PNP_DEV(0x2e, 0)
+#define SERIAL_DEV PNP_DEV(0x2e, NCT6776_SP1)
+#define ACPI_DEV   PNP_DEV(0x2e, NCT6776_ACPI)
+
+void bootblock_mainboard_early_init(void)
+{
+	nuvoton_pnp_enter_conf_state(GLOBAL_DEV);
+
+	/* Select SIO pin states */
+	pnp_write_config(GLOBAL_DEV, 0x1a, 0xc8);
+	pnp_write_config(GLOBAL_DEV, 0x1b, 0x06);
+	pnp_write_config(GLOBAL_DEV, 0x1c, 0x83);
+	pnp_write_config(GLOBAL_DEV, 0x24, 0x34);
+	pnp_write_config(GLOBAL_DEV, 0x27, 0x40);
+	pnp_write_config(GLOBAL_DEV, 0x2a, 0x20);
+	pnp_write_config(GLOBAL_DEV, 0x2c, 0x80);
+	pnp_write_config(GLOBAL_DEV, 0x2f, 0x01);
+
+	/* Power RAM in S3 */
+	pnp_set_logical_device(ACPI_DEV);
+	pnp_write_config(ACPI_DEV, 0xe4, 0x10);
+
+	nuvoton_pnp_exit_conf_state(GLOBAL_DEV);
+
+	nuvoton_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
+}
---
+++ src/mainboard/asus/h61-series/variants/p8h61-m_lx2/hda_verb.c
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+static const u32 via_vt1708s_verbs[] = {
+	AZALIA_SUBVENDOR(0, 0x10438415),
+	AZALIA_PIN_CFG(0, 0x19, 0x410110f0),
+	AZALIA_PIN_CFG(0, 0x1a, 0x01a19036),
+	AZALIA_PIN_CFG(0, 0x1b, 0x0181303e),
+	AZALIA_PIN_CFG(0, 0x1c, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x1d, 0x0221401f),
+	AZALIA_PIN_CFG(0, 0x1e, 0x02a19037),
+	AZALIA_PIN_CFG(0, 0x1f, 0x503701f0),
+	AZALIA_PIN_CFG(0, 0x20, 0x185600f0),
+	AZALIA_PIN_CFG(0, 0x21, 0x474411f0),
+	AZALIA_PIN_CFG(0, 0x22, 0x410160f0),
+	AZALIA_PIN_CFG(0, 0x23, 0x410120f0),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "VIA VT1708S",
+		.vendor_id    = 0x11060397,
+		.subsystem_id = 0x10438415,
+		.address      = 0,
+		.verbs        = via_vt1708s_verbs,
+		.verb_count   = ARRAY_SIZE(via_vt1708s_verbs),
+	},
+	{ /* terminator */ }
+};
+
+AZALIA_ARRAY_SIZES;
