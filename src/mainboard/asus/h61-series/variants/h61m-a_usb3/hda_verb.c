054318251cffe06b4168c2ae453d0c443f4d864b
Angel Pons <th3fanbus@gmail.com>
2026-03-31 18:15:08 +0200

    mb/asus/h61m-a_usb3/hda_verb.c: Drop extraneous codec verbs
    Apparently I used autoport to make logs for an Alder Lake system (even
    though autoport does not support Alder Lake as of writing), and one of
    the codec files remained in the logs folder when I did the port of the
    Asus H61M-A USB3. This was confirmed by checking the timestamps of the
    files in the logs folder (last used to port the Asus H61M-A USB3).
    
    Change-Id: Ica7156153fd2e50d9488446cd4be2eaf8cea3c8d
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92180
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-by: Nicholas Sudsgaard <devel+coreboot@nsudsgaard.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/mainboard/asus/h61-series/variants/h61m-a_usb3/hda_verb.c
+++ src/mainboard/asus/h61-series/variants/h61m-a_usb3/hda_verb.c
@@ -21,20 +21,7 @@ static const u32 realtek_alc887_verbs[] = {
 	AZALIA_PIN_CFG(0, ALC887_SPDIF_IN, AZALIA_PIN_CFG_NC(0)),
 };
 
-static const u32 intel_display_audio_1_verbs[] = {
-	AZALIA_SUBVENDOR(2, 0x80860101),
-	AZALIA_PIN_CFG(2, 0x04, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x06, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x08, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0a, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0b, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0c, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0d, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0e, 0x18560010),
-	AZALIA_PIN_CFG(2, 0x0f, 0x18560010),
-};
-
-static const u32 intel_display_audio_2_verbs[] = {
+static const u32 intel_display_audio_verbs[] = {
 	AZALIA_SUBVENDOR(3, 0x80860101),
 	AZALIA_PIN_CFG(3, 0x05, 0x58560010),
 	AZALIA_PIN_CFG(3, 0x06, 0x58560020),
@@ -53,29 +40,12 @@ struct azalia_codec mainboard_azalia_codecs[] = {
 		.verb_count   = ARRAY_SIZE(realtek_alc887_verbs),
 	},
 	{
-		/*
-		 * TODO: This seems out of place, the vendor ID suggests this
-		 * codec is an Alder Lake P Intel Display Audio[1], and
-		 * the user manual[2] does not mention this codec. Could someone
-		 * with this board confirm what this is?
-		 *
-		 * [1] Linux kernel: sound/hda/codecs/hdmi/intelhdmi.c:789
-		 * [2] H61M-A/USB3 User's Manual (English), Version E8184
-		 */
-		.name         = "Intel Display Audio #1 (HDMI/DP)",
-		.vendor_id    = 0x8086281c,
-		.subsystem_id = 0x80860101,
-		.address      = 2,
-		.verbs        = intel_display_audio_1_verbs,
-		.verb_count   = ARRAY_SIZE(intel_display_audio_1_verbs),
-	},
-	{
-		.name         = "Intel Display Audio #2 (HDMI/DP)",
+		.name         = "Intel Display Audio (HDMI/DP)",
 		.vendor_id    = 0x80862805,
 		.subsystem_id = 0x80860101,
 		.address      = 3,
-		.verbs        = intel_display_audio_2_verbs,
-		.verb_count   = ARRAY_SIZE(intel_display_audio_2_verbs),
+		.verbs        = intel_display_audio_verbs,
+		.verb_count   = ARRAY_SIZE(intel_display_audio_verbs),
 	},
 	{ /* terminator */ }
 };
