a3bf18f3b26b02bfaea0ef229f6edb5144388226
Kapil Porwal <kapilporwal@google.com>
2026-04-02 17:52:36 +0530

    soc/qualcomm/common: Add APIs to configure PMIC GPIOs
    Currently, PMIC GPIO configuration is often handled by manual SPMI
    writes in various parts of the codebase.
    
    Introduce a common PMIC GPIO driver with APIs to configure GPIO
    mode, source, and enable status. This provides a standardized way
    to manage PMIC-attached peripherals and simplifies board-level
    initialization code.
    
    BUG=none
    TEST=Verify new API on Google/Quartz.
    
    Change-Id: I3f1f3458cbeeb51644fa2e528afec783182c5ebb
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/91974
    Reviewed-by: Subrata Banik <subratabanik@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

---
+++ src/soc/qualcomm/common/include/soc/pmic_gpio.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef _SOC_QUALCOMM_PMIC_GPIO_H_
+#define _SOC_QUALCOMM_PMIC_GPIO_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <types.h>
+
+#define PMIC_GPIO_BASE(num)		(0x8800 + ((num - 1) * 0x100))
+#define PMIC_GPIO_NUMBER_MIN		1
+#define PMIC_GPIO_NUMBER_MAX		12
+
+#define PMIC_GPIO_MODE_CTL		0x40
+ #define PMIC_GPIO_MODE_OUTPUT		0x01
+ #define PMIC_GPIO_MODE_INPUT		0x00
+
+#define PMIC_GPIO_DIG_VIN_CTL		0x41
+ #define PMIC_GPIO_DIG_VIN_VIN0		0x00
+
+#define PMIC_GPIO_DIG_PULL_CTL		0x42
+
+#define PMIC_GPIO_DIG_OUT_SOURCE_CTL	0x44
+ #define PMIC_GPIO_DIG_OUT_SOURCE_HIGH	0x80
+ #define PMIC_GPIO_DIG_OUT_SOURCE_LOW	0x00
+
+#define PMIC_GPIO_DIG_OUT_CTL		0x45
+
+#define PMIC_GPIO_EN_CTL		0x46
+ #define PMIC_GPIO_EN_PERPH_EN		0x80
+
+void pmic_gpio_configure(uint8_t sid, uint8_t gpio_num,
+				uint8_t source, uint8_t enable,
+				uint8_t mode);
+
+void pmic_gpio_output(uint8_t sid, uint8_t gpio_num, bool high);
+
+#endif	// _SOC_QUALCOMM_PMIC_GPIO_H_
---
+++ src/soc/qualcomm/common/pmic_gpio.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <arch/mmio.h>
+#include <commonlib/bsd/stdlib.h>
+#include <soc/pmic_gpio.h>
+#include <soc/qcom_spmi.h>
+
+void pmic_gpio_configure(uint8_t sid, uint8_t gpio_num,
+				uint8_t source, uint8_t enable,
+				uint8_t mode)
+{
+	if (gpio_num < PMIC_GPIO_NUMBER_MIN || gpio_num > PMIC_GPIO_NUMBER_MAX) {
+		printk(BIOS_ERR, "Invalid PMIC GPIO (%d:%d)", sid, gpio_num);
+		return;
+	}
+
+	spmi_write8(SPMI_ADDR(sid, PMIC_GPIO_BASE(gpio_num) + PMIC_GPIO_DIG_OUT_SOURCE_CTL),
+			source);
+	spmi_write8(SPMI_ADDR(sid, PMIC_GPIO_BASE(gpio_num) + PMIC_GPIO_EN_CTL),
+			enable);
+	spmi_write8(SPMI_ADDR(sid, PMIC_GPIO_BASE(gpio_num) + PMIC_GPIO_MODE_CTL),
+			mode);
+}
+
+void pmic_gpio_output(uint8_t sid, uint8_t gpio_num, bool high)
+{
+	uint8_t state = high ? PMIC_GPIO_DIG_OUT_SOURCE_HIGH : PMIC_GPIO_DIG_OUT_SOURCE_LOW;
+	pmic_gpio_configure(sid, gpio_num, state, PMIC_GPIO_EN_PERPH_EN, PMIC_GPIO_MODE_OUTPUT);
+}
