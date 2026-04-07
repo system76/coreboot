08bff09608f4963a445d6c44fb6b8fea48e6e6cb
Maximilian Brune <maximilian.brune@9elements.com>
2026-04-07 19:57:35 +0200

    vc/amd/fsp/renoir/FspUpd.h: Fix comment for FSP signatures
    Comments can be treacherous. So update it to make it less so.
    But fear not, since the binaries are identical.
    
    Signed-off-by: Maximilian Brune <maximilian.brune@9elements.com>
    Change-Id: I93d0fab63a424998600172af310ffe294c317c3e
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92044
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>

--- src/vendorcode/amd/fsp/renoir/FspUpd.h
+++ src/vendorcode/amd/fsp/renoir/FspUpd.h
@@ -14,7 +14,7 @@
 # include <fsp_h_c99.h>
 #endif
 
-#define FSPM_UPD_SIGNATURE               0x4D5F31305F444D41        /* 'RENOIR_M' */
-#define FSPS_UPD_SIGNATURE               0x535F31305F444D41        /* 'RENOIR_S' */
+#define FSPM_UPD_SIGNATURE               0x4D5F31305F444D41        /* 'AMD_01_M' */
+#define FSPS_UPD_SIGNATURE               0x535F31305F444D41        /* 'AMD_01_S' */
 
 #endif
