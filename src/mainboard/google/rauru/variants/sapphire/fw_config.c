c39b0318de856a0da89f66dfe65c481744cf979d
Yu-Ping Wu <yupingso@google.com>
2026-04-13 11:38:35 +0800

    mb/google/rauru/sapphire: Override PANEL_ID in fw_config
    Implement `fw_config_get_mainboard_override` for the Sapphire variant to
    dynamically override the `PANEL_ID` field in the in-memory `fw_config`
    based on the hardware panel ID probed via ADC pins.
    
    This guarantees that the correct device tree is selected for the
    connected panel, even on unprovisioned devices or devices with an
    outdated CBI.
    
    BUG=b:467885981
    TEST=emerge-tanjiro coreboot
    BRANCH=none
    
    Change-Id: I80fd98805c1d1bfff4799906aa82d032fb1e5e84
    Signed-off-by: Yu-Ping Wu <yupingso@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92240
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Chen-Tsung Hsieh <chentsung@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>

--- src/mainboard/google/rauru/variants/sapphire/fw_config.c
+++ src/mainboard/google/rauru/variants/sapphire/fw_config.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only OR MIT */
 
+#include <baseboard/panel.h>
+#include <console/console.h>
 #include <fw_config.h>
 #include <variants.h>
 
@@ -9,4 +11,18 @@ enum audio_amplifier_id get_audio_amp_id(void)
 		return AUD_AMP_ID_TAS2563;
 
 	return AUD_AMP_ID_UNKNOWN;
+}
+
+void fw_config_get_mainboard_override(uint64_t *fw_config)
+{
+	/* Handle unprovisioned fw_config. */
+	if (*fw_config == UNDEFINED_FW_CONFIG) {
+		/* Reset fw_config to a default value with boot-necessary fields.
+		   For now, setting to 0 is sufficient. */
+		*fw_config = 0;
+	}
+
+	uint32_t id = panel_id();
+	printk(BIOS_INFO, "Overriding fw_config PANEL_ID with %u\n", id);
+	fw_config_value_set_field(fw_config, FW_CONFIG_FIELD(PANEL_ID), id);
 }
