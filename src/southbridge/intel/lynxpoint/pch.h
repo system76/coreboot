dccf924a2c49ae161c697adcd1bf12237cc22673
Angel Pons <th3fanbus@gmail.com>
2026-04-19 16:24:59 +0200

    sb/intel/lynxpoint: Split a few things off pch.h
    The plan is to reference this new `pch_minimal.h` file from wildcatpoint
    to aid in the unification process. When the unification is complete, the
    file will then be removed.
    
    Change-Id: I6b0829a0d5945e64587cc34b9e1be1eab15ba279
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92294
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/southbridge/intel/lynxpoint/pch.h
+++ src/southbridge/intel/lynxpoint/pch.h
@@ -6,6 +6,9 @@
 #include <acpi/acpi.h>
 #include <southbridge/intel/common/rcba.h> /* IWYU pragma: export */
 
+/* TODO: Temporary to help unify Lynx Point and Wildcat Point, drop afterwards */
+#include <southbridge/intel/lynxpoint/pch_minimal.h> /* IWYU pragma: export */
+
 #define CROS_GPIO_DEVICE_NAME	"LynxPoint"
 
 /*
@@ -46,76 +49,7 @@
 
 #define SMBUS_SLAVE_ADDR	0x24
 
-#if CONFIG(INTEL_LYNXPOINT_LP)
-#define DEFAULT_PMBASE		0x1000
-#define DEFAULT_GPIOBASE	0x1400
-#define DEFAULT_GPIOSIZE	0x400
-#else
-#define DEFAULT_PMBASE		0x500
-#define DEFAULT_GPIOBASE	0x480
-#define DEFAULT_GPIOSIZE	0x80
-#endif
-
 #ifndef __ACPI__
-
-#if CONFIG(INTEL_LYNXPOINT_LP)
-#define MAX_USB2_PORTS	10
-#define MAX_USB3_PORTS	4
-#else
-#define MAX_USB2_PORTS	14
-#define MAX_USB3_PORTS	6
-#endif
-
-/* There are 8 OC pins */
-#define USB_OC_PIN_SKIP	8
-
-enum usb2_port_location {
-	USB_PORT_SKIP = 0,
-	USB_PORT_BACK_PANEL,
-	USB_PORT_FRONT_PANEL,
-	USB_PORT_DOCK,
-	USB_PORT_MINI_PCIE,
-	USB_PORT_FLEX,
-	USB_PORT_INTERNAL,
-};
-
-/*
- * USB port length is in MRC format: binary-coded decimal length in tenths of an inch.
- *   4.2 inches -> 0x0042
- *  12.7 inches -> 0x0127
- */
-struct usb2_port_config {
-	uint16_t length;
-	bool enable;
-	unsigned short oc_pin;
-	enum usb2_port_location location;
-};
-
-struct usb3_port_config {
-	bool enable;
-	unsigned int oc_pin;
-	/*
-	 * Set to 0 if trace length is > 5 inches
-	 * Set to 1 if trace length is <= 5 inches
-	 */
-	uint8_t fixed_eq;
-};
-
-/* Mainboard-specific USB configuration */
-extern const struct usb2_port_config mainboard_usb2_ports[MAX_USB2_PORTS];
-extern const struct usb3_port_config mainboard_usb3_ports[MAX_USB3_PORTS];
-
-static inline bool pch_is_lp(void)
-{
-	return CONFIG(INTEL_LYNXPOINT_LP);
-}
-
-/* PCH platform types, safe for MRC consumption */
-enum pch_platform_type {
-	PCH_TYPE_MOBILE	 = 0,
-	PCH_TYPE_DESKTOP = 1, /* or server */
-	PCH_TYPE_ULT	 = 5,
-};
 
 void pch_dmi_setup_physical_layer(void);
 void pch_dmi_tc_vc_mapping(u32 vc0, u32 vc1, u32 vcp, u32 vcm);
@@ -170,10 +104,6 @@ void pch_enable_lpc(void);
 void uart_bootblock_init(void);
 void mainboard_config_superio(void);
 void mainboard_config_rcba(void);
-
-#define MAINBOARD_POWER_OFF	0
-#define MAINBOARD_POWER_ON	1
-#define MAINBOARD_POWER_KEEP	2
 
 /* PCI Configuration Space (D30:F0): PCI2PCI */
 #define PSTS	0x06
