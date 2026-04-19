8a50ec739b9ca0ea933f68de0a3a31d93d5c5443
Angel Pons <th3fanbus@gmail.com>
2026-04-19 13:22:32 +0200

    nb/intel/gm45: Name IOMMU registers and addresses
    Intel document 320122-006 (Mobile Intel® 4 Series Express Chipset
    Family Datasheet) contains names for the IOMMU remap base address
    registers in MCHBAR. Define macros for these registers and rename
    the `IOMMU_BASEx` macros accordingly.
    
    Tested with BUILD_TIMELESS=1, Lenovo T400 remains identical.
    
    Change-Id: Iececcd83ae076f55543ff9cdd155cf1dd2c2efb1
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92278
    Reviewed-by: Nicholas <nic.c3.14@gmail.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/northbridge/intel/gm45/iommu.c
+++ src/northbridge/intel/gm45/iommu.c
@@ -7,27 +7,32 @@
 
 #include "gm45.h"
 
+#define MEREMAPBAR	0x10
+#define GFXREMAPBAR	0x18
+#define VC0REMAPBAR	0x20
+#define VC1REMAPBAR	0x28
+
 void init_iommu(void)
 {
 	/* FIXME: proper test? */
 	int me_active = pci_read_config8(PCI_DEV(0, 3, 0), PCI_CLASS_REVISION) != 0xff;
 	int stepping = pci_read_config8(PCI_DEV(0, 0, 0), PCI_CLASS_REVISION);
 
-	mchbar_write32(0x28, IOMMU_BASE1 | 1); /* HDA @ 0:1b.0 */
+	mchbar_write32(VC1REMAPBAR, IOMMU_BASE_VC1 | 1); /* HDA @ 0:1b.0 */
 	if (stepping != STEPPING_B2) {
 		/* The official workaround is to run SMM every 64ms.
 		   The only winning move is not to play. */
-		mchbar_write32(0x18, IOMMU_BASE2 | 1); /* IGD @ 0:2.0-1 */
+		mchbar_write32(GFXREMAPBAR, IOMMU_BASE_GFX | 1); /* IGD @ 0:2.0-1 */
 	} else {
 		/* write-once, so lock it down */
-		mchbar_write32(0x18, 0); /* disable IOMMU for IGD @ 0:2.0-1 */
+		mchbar_write32(GFXREMAPBAR, 0); /* disable IOMMU for IGD @ 0:2.0-1 */
 	}
 	if (me_active) {
-		mchbar_write32(0x10, IOMMU_BASE3 | 1); /* ME  @ 0:3.0-3 */
+		mchbar_write32(MEREMAPBAR, IOMMU_BASE_ME | 1); /* ME  @ 0:3.0-3 */
 	} else {
-		mchbar_write32(0x10, 0); /* disable IOMMU for ME */
+		mchbar_write32(MEREMAPBAR, 0); /* disable IOMMU for ME */
 	}
-	mchbar_write32(0x20, IOMMU_BASE4 | 1); /* all other DMA sources */
+	mchbar_write32(VC0REMAPBAR, IOMMU_BASE_VC0 | 1); /* all other DMA sources */
 
 	if (stepping == STEPPING_B3) {
 		mchbar_setbits8(0xffc, 1 << 4);
