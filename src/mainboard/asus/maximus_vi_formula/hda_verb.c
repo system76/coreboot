74dda0b0a0ed015412f1485348a3788ecf701615
Jan Philipp Groß <jeangrande@mailbox.org>
2026-04-20 23:49:21 +0200

    mb/asus: Add Maximus VI Formula (Haswell)
    This port was done via autoport and subsequent manual tweaking.
    
    Working:
    - Devil's Canyon CPUs (tested with i7-4790K with MRC and NRI)
    - All four DDR3 DIMM slots (tested with Kingston HX324C11SRK2/16
      and G.SKILL F3-2400C11D-16GAB on MRC and NRI)
    - All video ports
    - Gigabit LAN Port
    - All USB ports and Headers
    - All SATA 3 ports
    - All PCIe slots
    - HD audio jack (audio output tested only)
    - TPM 2.0 (tested with Infineon SLB9665TT20)
    - S3 Suspend and Resume
    - UEFI Secureboot
    
    Not (yet) working:
    - Front audio header: no audio comes out of it. Might be because
      the EFP035 TPU chip is holding the Cirrus CS4398 DAC in reset?
    
    Not (yet) tested:
    - PS/2 port
    - NGFF slot
    
    Change-Id: I6286e5a044af0a8cf272cbd35a2e7cd93429bde7
    Signed-off-by: Jan Philipp Groß <jeangrande@mailbox.org>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92317
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Angel Pons <th3fanbus@gmail.com>

---
+++ src/mainboard/asus/maximus_vi_formula/hda_verb.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <device/azalia_device.h>
+
+static const u32 realtek_alc1150_verbs[] = {
+	AZALIA_SUBVENDOR(0, 0x1043857e),
+	AZALIA_PIN_CFG(0, 0x11, 0x99430130),
+	AZALIA_PIN_CFG(0, 0x14, 0x01014010),
+	AZALIA_PIN_CFG(0, 0x15, 0x01011012),
+	AZALIA_PIN_CFG(0, 0x16, 0x01016011),
+	AZALIA_PIN_CFG(0, 0x17, 0x01012014),
+	AZALIA_PIN_CFG(0, 0x18, 0x01a19050),
+	AZALIA_PIN_CFG(0, 0x19, 0x02a19060),
+	AZALIA_PIN_CFG(0, 0x1a, 0x0181305f),
+	AZALIA_PIN_CFG(0, 0x1b, 0x02214020),
+	AZALIA_PIN_CFG(0, 0x1e, 0x01456140),
+};
+
+const u32 pc_beep_verbs[0] = {};
+
+struct azalia_codec mainboard_azalia_codecs[] = {
+	{
+		.name         = "Realtek ALC1150",
+		.vendor_id    = 0x10ec0900,
+		.subsystem_id = 0x1043857e,
+		.address      = 0,
+		.verbs        = realtek_alc1150_verbs,
+		.verb_count   = ARRAY_SIZE(realtek_alc1150_verbs),
+	},
+	{ /* terminator */ }
+};
+
+AZALIA_ARRAY_SIZES;
