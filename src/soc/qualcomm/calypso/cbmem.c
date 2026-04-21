10a2805216ad51947c1a464967c953fa5bc990d3
Subrata Banik <subratabanik@google.com>
2026-04-21 18:20:30 +0000

    soc/qualcomm/calypso: Implement cbmem_top_chipset()
    Currently, cbmem_top_chipset() is a stub that returns NULL and prints
    an error message. This prevents CBMEM from being properly initialized
    on Calypso platforms.
    
    Update the implementation to return _dram_pil, which marks the start of
    the region reserved for Peripheral Image Loader (PIL) blobs and defines
    the usable top of DRAM for coreboot.
    
    Change-Id: I32a23c0fbc26b72e69414ddd03ff6c78e53ca1af
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92337
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>

--- src/soc/qualcomm/calypso/cbmem.c
+++ src/soc/qualcomm/calypso/cbmem.c
@@ -1,10 +1,9 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <cbmem.h>
-#include <console/console.h>
+#include <soc/symbols_common.h>
 
 uintptr_t cbmem_top_chipset(void)
 {
-	printk(BIOS_ERR, "%s: Update CBMEM TOP address.\n", __func__);
-	return (uintptr_t)NULL;
+	return (uintptr_t)_dram_pil;
 }
