2790b09579f398b98c63983aa2b93deaf94ed8c9
Patrick Rudolph <patrick.rudolph@9elements.com>
2026-04-30 10:19:20 +0200

    util/amdfwtool: Use lookup table
    Convert the soc.c functions to use a lookup table and add comments
    to each function what it returns.
    
    TEST=Timeless build of 53 AMD board variants shows no binary difference.
    
    Change-Id: I8e14820cdbee59f5d83878e6644316ba9e0de4f0
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92469
    Reviewed-by: Felix Held <felix-coreboot@felixheld.de>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- util/amdfwtool/soc.c
+++ util/amdfwtool/soc.c
@@ -6,205 +6,265 @@
 
 #include "amdfwtool.h"
 
-/* This file contains the SoC quirks. TODO: Use lookup tables instead. */
+/* This file contains the SoC quirks. */
 
+struct platform_info {
+	const char *name;
+	bool needs_ish;
+	bool is_multi_level;
+	bool has_apob_nv_quirk;
+	bool is_initial_alignment_required;
+	bool is_second_gen;
+	bool has_dir_header_v1;
+	uint32_t psp_id;
+};
+
+static const struct platform_info platform_table[] = {
+	[PLATFORM_MULLINS] = {
+		.name = "Mullins",
+		.needs_ish = false,
+		.is_multi_level = false,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = false,
+		.has_dir_header_v1 = false,
+		.psp_id = 0,
+	},
+	[PLATFORM_CARRIZO] = {
+		.name = "Carrizo",
+		.needs_ish = false,
+		.is_multi_level = false,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = false,
+		.has_dir_header_v1 = false,
+		.psp_id = 0,
+	},
+	[PLATFORM_STONEYRIDGE] = {
+		.name = "Stoneyridge",
+		.needs_ish = false,
+		.is_multi_level = false,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = false,
+		.has_dir_header_v1 = false,
+		.psp_id = 0x10220B00,
+	},
+	[PLATFORM_RAVEN] = {
+		.name = "Raven",
+		.needs_ish = false,
+		.is_multi_level = false,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = false,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0A0000,
+	},
+	[PLATFORM_PICASSO] = {
+		.name = "Picasso",
+		.needs_ish = false,
+		.is_multi_level = false,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = false,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0A0000,
+	},
+	[PLATFORM_RENOIR] = {
+		.name = "Renoir",
+		.needs_ish = false,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0C0000,
+	},
+	[PLATFORM_LUCIENNE] = {
+		.name = "Lucienne",
+		.needs_ish = false,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0C0000,
+	},
+	[PLATFORM_CEZANNE] = {
+		.name = "Cezanne",
+		.needs_ish = false,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = true,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0C0140,
+	},
+	[PLATFORM_MENDOCINO] = {
+		.name = "Mendocino",
+		.needs_ish = true,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = true,
+		.is_initial_alignment_required = false,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0D0900,
+	},
+	[PLATFORM_PHOENIX] = {
+		.name = "Phoenix",
+		.needs_ish = true,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = false,
+		.is_initial_alignment_required = false,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0D0400,
+	},
+	[PLATFORM_STRIX] = {
+		.name = "Strix",
+		.needs_ish = true,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = false,
+		.is_initial_alignment_required = false,
+		.is_second_gen = true,
+		.has_dir_header_v1 = true,
+		.psp_id = 0xBC0E0200,
+	},
+	[PLATFORM_GENOA] = {
+		.name = "Genoa",
+		.needs_ish = false,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = false,
+		.is_initial_alignment_required = true,
+		.is_second_gen = true,
+		.has_dir_header_v1 = false,
+		.psp_id = 0xBC0C0111,
+	},
+	[PLATFORM_KRACKAN2E] = {
+		.name = "Krackan2e",
+		.needs_ish = true,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = false,
+		.is_initial_alignment_required = false,
+		.is_second_gen = true,
+		.has_dir_header_v1 = true,
+		.psp_id = 0xbc0e1000,
+	},
+	[PLATFORM_STRIXHALO] = {
+		.name = "Strixhalo",
+		.needs_ish = true,
+		.is_multi_level = true,
+		.has_apob_nv_quirk = false,
+		.is_initial_alignment_required = false,
+		.is_second_gen = true,
+		.has_dir_header_v1 = true,
+		.psp_id = 0xbc0e0900,
+	},
+};
+
+/**
+ * Identify the platform based on SoC name string.
+ *
+ * @param soc_name: Case-insensitive SoC name string to match
+ * @return: Matching platform enum value, or PLATFORM_UNKNOWN if no match found
+ */
 enum platform platform_identify(char *soc_name)
 {
-	if (!strcasecmp(soc_name, "Stoneyridge"))
-		return PLATFORM_STONEYRIDGE;
-	else if (!strcasecmp(soc_name, "Carrizo"))
-		return PLATFORM_CARRIZO;
-	else if (!strcasecmp(soc_name, "Mullins"))
-		return PLATFORM_MULLINS;
-	else if (!strcasecmp(soc_name, "Raven"))
-		return PLATFORM_RAVEN;
-	else if (!strcasecmp(soc_name, "Picasso"))
-		return PLATFORM_PICASSO;
-	else if (!strcasecmp(soc_name, "Cezanne"))
-		return PLATFORM_CEZANNE;
-	else if (!strcasecmp(soc_name, "Mendocino"))
-		return PLATFORM_MENDOCINO;
-	else if (!strcasecmp(soc_name, "Renoir"))
-		return PLATFORM_RENOIR;
-	else if (!strcasecmp(soc_name, "Lucienne"))
-		return PLATFORM_LUCIENNE;
-	else if (!strcasecmp(soc_name, "Phoenix"))
-		return PLATFORM_PHOENIX;
-	else if (!strcasecmp(soc_name, "Strix"))
-		return PLATFORM_STRIX;
-	else if (!strcasecmp(soc_name, "Genoa"))
-		return PLATFORM_GENOA;
-	else if (!strcasecmp(soc_name, "Krackan2e"))
-		return PLATFORM_KRACKAN2E;
-	else if (!strcasecmp(soc_name, "Strixhalo"))
-		return PLATFORM_STRIXHALO;
-	else
-		return PLATFORM_UNKNOWN;
+	for (size_t i = 0; i < ARRAY_SIZE(platform_table); i++) {
+		if (!platform_table[i].name) /* PLATFORM_UNKNOWN has no entry */
+			continue;
+		if (!strcasecmp(soc_name, platform_table[i].name))
+			return i;
+	}
+	return PLATFORM_UNKNOWN;
 }
 
+/**
+ * Check if the platform requires ISH (Image slot header) support.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if ISH is needed, false otherwise
+ */
 bool platform_needs_ish(enum platform platform_type)
 {
-	if (platform_type == PLATFORM_MENDOCINO || platform_type == PLATFORM_PHOENIX ||
-		platform_type == PLATFORM_STRIX || platform_type == PLATFORM_KRACKAN2E ||
-		platform_type == PLATFORM_STRIXHALO)
-		return true;
-	else
-		return false;
+	return platform_table[platform_type].needs_ish;
 }
 
-/* Returns true when the PSP requires 'PSP L1' and 'PSP L2' directory tables. */
+/**
+ * Check if the platform uses multi-level directory structure.
+ * Multi-level means there are separate PSP L1 and PSP L2 directory tables,
+ * as opposed to a flat structure with only one PSP directory.
+ *
+ * Combo directory tables do not count as multi-level since they are a different
+ * format and not supported.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if multi-level directory is used, false for flat structure
+ */
 bool platform_is_multi_level(enum platform platform_type)
 {
-	// FIXME: Add turin here as well.
-
-	/*
-	 * SoCs older than Family 17h don't support multi level PSP directories.
-	 *
-	 * Raven and Picasso only support multi-level through PSP Combo directories.
-	 * Combo directory generation isn't supported by this tool and different from
-	 * the later EFS -> PSP L1 -> PSP L2 multilevel layout.
-	 *
-	 * Starting from Cezanne 'One Level PSP Directory Layout' is deprecated.
-	 */
-	switch (platform_type) {
-	case PLATFORM_MENDOCINO:
-	case PLATFORM_PHOENIX:
-	case PLATFORM_STRIX:
-	case PLATFORM_KRACKAN2E:
-	case PLATFORM_STRIXHALO:
-	case PLATFORM_GENOA:
-	case PLATFORM_LUCIENNE:
-	case PLATFORM_CEZANNE:
-	case PLATFORM_RENOIR:
-		return true;
-	default:
-		return false;
-	}
+	return platform_table[platform_type].is_multi_level;
 }
 
-/*
- * For some SOC generations the APOB_NV binary seems to be treated special regarding the
- * interpretaion of the source address. No matter the address_mode specified for the address
- * the memory ABL always seems to the interpret the source address as MMIO address even if
- * AMD_ADDR_REL_BIOS is specified. So for them we need to always use an MMIO address.
- * This seems to be a bug which affects all SOCs before phoenix generation.
+/**
+ * Check if the platform has APOB NV (AMD PSP Output Block Non-Volatile) quirk.
+ * On older platforms that only had 16 MiB of ROM space or less, the address-mode was
+ * always physical and the PSP would do address translation internally by cutting off the upper bits.
+ *
+ * On newer platforms, the address-mode is always AMD_ADDR_REL_BIOS.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if APOB NV quirk is present, false otherwise
  */
 bool platform_has_apob_nv_quirk(enum platform platform_type)
 {
-	switch (platform_type) {
-	case PLATFORM_MULLINS:
-	case PLATFORM_CARRIZO:
-	case PLATFORM_STONEYRIDGE:
-	case PLATFORM_RAVEN:
-	case PLATFORM_PICASSO:
-	case PLATFORM_RENOIR:
-	case PLATFORM_CEZANNE:
-	case PLATFORM_MENDOCINO:
-	case PLATFORM_LUCIENNE:
-		return true;
-	default:
-		return false;
-	}
+	return platform_table[platform_type].has_apob_nv_quirk;
 }
 
+/**
+ * Check if the platform requires initial alignment for firmware layout.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if initial alignment is required, false otherwise
+ */
 bool platform_is_initial_alignment_required(enum platform platform_type)
 {
-	switch (platform_type) {
-	case PLATFORM_MENDOCINO:
-	case PLATFORM_PHOENIX:
-	case PLATFORM_STRIX:
-	case PLATFORM_KRACKAN2E:
-	case PLATFORM_STRIXHALO:
-		return false;
-	default:
-		return true;
-	}
+	return platform_table[platform_type].is_initial_alignment_required;
 }
 
+/**
+ * Check if the platform is second generation EFS structure.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if second generation, false for first generation
+ */
 bool platform_is_second_gen(enum platform platform_type)
 {
-	switch (platform_type) {
-	case PLATFORM_MULLINS:
-	case PLATFORM_CARRIZO:
-	case PLATFORM_STONEYRIDGE:
-	case PLATFORM_RAVEN:
-	case PLATFORM_PICASSO:
-		return false;
-	case PLATFORM_RENOIR:
-	case PLATFORM_LUCIENNE:
-	case PLATFORM_CEZANNE:
-	case PLATFORM_MENDOCINO:
-	case PLATFORM_PHOENIX:
-	case PLATFORM_STRIX:
-	case PLATFORM_GENOA:
-	case PLATFORM_KRACKAN2E:
-	case PLATFORM_STRIXHALO:
-		return true;
-	case PLATFORM_UNKNOWN:
-	default:
-		fprintf(stderr, "Error: Invalid SOC name.\n\n");
-		return false;
-	}
+	return platform_table[platform_type].is_second_gen;
 }
 
-/*
- * Returns true when the PSP supports the Additional Information Field v1
- * in the directory header. Allows to support directories bigger than
- * 4 MiB in total.
+/**
+ * Check if the platform uses PSP directory header version 1 format.
+ * If false it uses the PSP directory header version 0 and supports a maximum
+ * of 4 MiB directory size, while version 1 supports up to 64 MiB.
+ *
+ * @param platform_type: Platform enum to check
+ * @return: true if dir header v1 is used, false for other versions
  */
 bool platform_has_dir_header_v1(enum platform platform_type)
 {
-	switch (platform_type) {
-	case PLATFORM_STRIX:
-	case PLATFORM_KRACKAN2E:
-	case PLATFORM_STRIXHALO:
-		return true;
-	default:
-		return false;
-	}
+	return platform_table[platform_type].has_dir_header_v1;
 }
 
+/**
+ * Get the PSP (Platform Security Processor) ID for the platform.
+ * Older platforms might not insert a PSP ID in the directory, in which case
+ * this function returns 0.
+ *
+ * @param platform_type: Platform enum to query
+ * @return: 32-bit PSP ID value, or 0 if not applicable
+ */
 uint32_t platform_get_psp_id(enum platform platform_type)
 {
-	uint32_t psp_id;
-	switch (platform_type) {
-	case PLATFORM_RAVEN:
-	case PLATFORM_PICASSO:
-		psp_id = 0xBC0A0000;
-		break;
-	case PLATFORM_RENOIR:
-	case PLATFORM_LUCIENNE:
-		psp_id = 0xBC0C0000;
-		break;
-	case PLATFORM_CEZANNE:
-		psp_id = 0xBC0C0140;
-		break;
-	case PLATFORM_MENDOCINO:
-		psp_id = 0xBC0D0900;
-		break;
-	case PLATFORM_STONEYRIDGE:
-		psp_id = 0x10220B00;
-		break;
-	case PLATFORM_STRIX:
-		psp_id = 0xBC0E0200;
-		break;
-	case PLATFORM_PHOENIX:
-		psp_id = 0xBC0D0400;
-		break;
-	case PLATFORM_GENOA:
-		psp_id = 0xBC0C0111;
-		break;
-	case PLATFORM_KRACKAN2E:
-		psp_id = 0xbc0e1000;
-		break;
-	case PLATFORM_STRIXHALO:
-		psp_id = 0xbc0e0900;
-		break;
-	case PLATFORM_CARRIZO:
-	case PLATFORM_MULLINS:
-	default:
-		psp_id = 0;
-		break;
-	}
-	return psp_id;
+	return platform_table[platform_type].psp_id;
 }
