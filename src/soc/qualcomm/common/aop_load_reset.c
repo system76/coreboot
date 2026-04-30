883a890ae3b21f0085d692682986d7db1f1ad21e
Subrata Banik <subratabanik@google.com>
2026-05-01 00:59:26 +0530

    soc/qualcomm/common: Map AOP region as cached during firmware load
    The AOP firmware loading process requires the destination address to
    be 4-byte aligned. By mapping the AOP RAM region as cached memory
    during the load, we ensure the alignment requirements are met and
    can take advantage of faster memory access during the transfer.
    
    The flow is modified to:
    1. Map the AOP code RAM region as CACHED_RAM.
    2. Load the AOP firmware via selfload().
    3. Clean the dcache by MVA to ensure data is written to memory.
    4. Remap the region back to DEV_MEM for execution/operation.
    
    Change-Id: I54d9a74b4aa10a36d4b951328fca28234b3cff51
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/92487
    Reviewed-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-by: Jayvik Desai <jayvik@google.com>
    Reviewed-by: Avi Uday <aviuday@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>

--- src/soc/qualcomm/common/aop_load_reset.c
+++ src/soc/qualcomm/common/aop_load_reset.c
@@ -1,18 +1,31 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <arch/cache.h>
+#include <arch/mmu.h>
 #include <console/console.h>
 #include <program_loading.h>
 #include <soc/mmu.h>
 #include <soc/aop_common.h>
 #include <soc/clock.h>
+#include <soc/mmu_common.h>
+#include <soc/symbols_common.h>
+#include <symbols.h>
 
 void aop_fw_load_reset(void)
 {
+	/* map to cached region to force address to be 4 byte aligned */
+	mmu_config_range((void *)_aop_code_ram, REGION_SIZE(aop_code_ram), CACHED_RAM);
+
 	struct prog aop_fw_prog =
 		PROG_INIT(PROG_PAYLOAD, CONFIG_CBFS_PREFIX "/aop");
 
 	if (!selfload(&aop_fw_prog))
 		die("SOC image: AOP load failed");
+
+	/* flush cached region */
+	dcache_clean_by_mva(_aop_code_ram, REGION_SIZE(aop_code_ram));
+	/* remap back to device memory */
+	mmu_config_range((void *)_aop_code_ram, REGION_SIZE(aop_code_ram), DEV_MEM);
 
 	clock_reset_aop();
 }
