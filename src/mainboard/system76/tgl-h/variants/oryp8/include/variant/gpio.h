8c76fb0af33ce8342cc438da6464377119b32aa7
Tim Crawford <tcrawford@system76.com>
2026-07-13 13:00:10 -0600

    mb/system76: Enable dGPUs
    Change-Id: I4ca91ff631dd4badbfba72e69651f03753323a54
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

---
+++ src/mainboard/system76/tgl-h/variants/oryp8/include/variant/gpio.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#ifndef VARIANT_GPIO_H
+#define VARIANT_GPIO_H
+
+#include <soc/gpio.h>
+
+#define DGPU_RST_N	GPP_F8
+#define DGPU_PWR_EN	GPP_F9
+#define DGPU_PWRGD	GPP_K2
+#define DGPU_GC6	GPP_K11
+#define DGPU_SSID	0x65f11558
+
+#ifndef __ACPI__
+void variant_configure_early_gpios(void);
+void variant_configure_gpios(void);
+#endif
+
+#endif
